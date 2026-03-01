f60a56bc3e4c919d026a8fa65bb3dd2c6b31d402
xyenrae <arifeenafif42@gmail.com>
2026-03-01 10:18:30 +0700

    change default theme to light

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body className={`${geistSans.className} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
